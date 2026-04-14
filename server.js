af01de7956c4a6ec833db09920bc1b4208b76fdc
FerdNanda <fenandalaia@outlook.com>
2026-04-14 08:46:51 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const { criarBanco } = require('./database');
+
+
+
+const app = express();
+
+app.use(express.json());
+
+
+
+app.get('/', (req, res) => {
+    res.send('Bem-vindo à API de Localização de Animais!')
+});
+
+app.get('/ongs', async (req, res) => {
+
+       const db = await criarBanco();
+
+       const ongs = await db.all('SELECT * FROM ongs');
+  
+     res.json(ongs);
+    
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Servidor rodando na porta  http://localhost:${PORT}`));
