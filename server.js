00bcd6c98b6a5b0bbb08c3f4b82ba8c1f4094829
FerdNanda <fenandalaia@outlook.com>
2026-04-18 14:22:09 -0300

    configura CORS e porta dinâmica para deploy

--- server.js
+++ server.js
@@ -19,9 +19,11 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () =>
-  console.log(`Servidor rodando na porta  http://localhost:${PORT}`),
-);
+app.listen(PORT, () => {
+  
+  console.log(`Servidor rodando na porta  http://localhost:${PORT}`);
+});
+
 
 //===============================LISTAGEM DE ONGS
 
